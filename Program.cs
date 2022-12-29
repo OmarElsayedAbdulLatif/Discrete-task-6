9441fe86e6ba444baca11d25d1a3c972bd726e16
Ømâr <120733338+OmarElsayedAbdulLatif@users.noreply.github.com>
2022-12-29 23:11:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Prime_numbers1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int numbers = int.Parse(Console.ReadLine());
+
+            int j;
+            bool current = false;
+
+            for (int i = 2; i <= numbers; i++)
+            {
+                for (j =2; j < i; j++)
+                {
+
+                    if (i % j == 0)
+                    {
+                        current = true;
+                    }
+                } 
+                if (current == false)
+                {
+                    Console.WriteLine("the PrimeNumbers Are {0} ", j);
+                }
+
+                else current = false;
+            }
+
+        }
+    }
+}
